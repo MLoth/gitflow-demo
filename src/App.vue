c11c654bbb46c7f9f85e770de241a53a6948c06d
Marty <MLoth@users.noreply.github.com>
2024-11-06 10:15:08 +0100

    feat: added icon to header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <main class="min-h-screen flex flex-col">
     <div class="max-w-screen-md mx-auto p-9">
-      <h1 class="text-4xl font-bold mb-1">Gitflow in actie</h1>
+      <h1 class="flex items-center gap-1 text-4xl font-bold mb-1">
+        <GitBranch />
+      Gitflow in actie
+      </h1>
       <p class="font-mono mb-8">
         {{ version }}
       </p>
@@ -110,5 +113,5 @@
 <script setup lang="ts">
 import { Github, GitGraph } from 'lucide-vue-next'
 import { version } from '../package.json'
-import { ExternalLink } from 'lucide-vue-next'
+import { ExternalLink, GitBranch } from 'lucide-vue-next'
 </script>
